f11ca50d5f6ac98cc0e4f7c2712aa59ba24e50e0
nakalua <luanadez13@gmail.com>
2020-09-09 07:42:45 -0300

    feat: open and close mobile-nav

--- my_project/shared.js
+++ my_project/shared.js
@@ -1,20 +1,33 @@
 var backdrop = document.querySelector('.backdrop')
 var modal = document.querySelector('.modal')
 var selectPlanButtons = document.querySelectorAll('.plan button')
-var noModalButton = document.querySelector('.modal button')
+var modalNoButton = document.querySelector('.modal button')
+var toggleButton = document.querySelector('.toggle-button')
+var mobileNav = document.querySelector('.mobile-nav')
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block'
-        backdrop.style.display = 'block'
+        // modal.style.display = 'block'
+        // backdrop.style.display = 'block'
+        // modal.className = 'open' //Thid will actually overwrite the conmplete class
+        modal.classList.add('open')
+        backdrop.classList.add('open')
     })
 }
 
-backdrop.addEventListener('click', closeModal)
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+})
 
-noModalButton.addEventListener('click', closeModal)
+modalNoButton.addEventListener('click', closeModal)
 
 function closeModal() {
     modal.style.display = 'none'
     backdrop.style.display = 'none'
 }
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block',
+    backdrop.style.display = 'block'
+})
