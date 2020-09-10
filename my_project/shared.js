91e8e29be08fec665650a035ff7aaac62d51d84b
nakalua <luanadez13@gmail.com>
2020-09-10 07:47:18 -0300

    feat: toggle menu button to all pages/refactor: shared.js

--- my_project/shared.js
+++ my_project/shared.js
@@ -6,28 +6,38 @@ var toggleButton = document.querySelector('.toggle-button')
 var mobileNav = document.querySelector('.mobile-nav')
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function() {
+    selectPlanButtons[i].addEventListener('click', function () {
         // modal.style.display = 'block'
         // backdrop.style.display = 'block'
-        // modal.className = 'open' //Thid will actually overwrite the conmplete class
+        // modal.className = 'open' //This will actually overwrite the conmplete class
         modal.classList.add('open')
         backdrop.classList.add('open')
     })
 }
 
-backdrop.addEventListener('click', function() {
-    mobileNav.style.display = 'none';
+backdrop.addEventListener('click', function () {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open')
     closeModal();
 })
 
-modalNoButton.addEventListener('click', closeModal)
-
-function closeModal() {
-    modal.style.display = 'none'
-    backdrop.style.display = 'none'
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal)
 }
 
-toggleButton.addEventListener('click', function() {
-    mobileNav.style.display = 'block',
-    backdrop.style.display = 'block'
+function closeModal() {
+    // modal.style.display = 'none'
+    // backdrop.style.display = 'none'
+    
+    if (modal) {
+        modal.classList.remove('open')
+    }
+    backdrop.classList.remove('open')
+}
+
+toggleButton.addEventListener('click', function () {
+    // mobileNav.style.display = 'block',
+    // backdrop.style.display = 'block'
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
 })
