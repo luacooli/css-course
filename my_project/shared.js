f7d55eb02263db7cb480bfb05c66d1ad198d749e
Luana <luanadez13@gmail.com>
2020-11-12 07:34:36 -0300

    feat: package animation and start animation on js

--- my_project/shared.js
+++ my_project/shared.js
@@ -1,53 +1,66 @@
-var backdrop = document.querySelector('.backdrop')
-var modal = document.querySelector('.modal')
-var selectPlanButtons = document.querySelectorAll('.plan button')
-var modalNoButton = document.querySelector('.modal button')
-var toggleButton = document.querySelector('.toggle-button')
-var mobileNav = document.querySelector('.mobile-nav')
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var modalNoButton = document.querySelector(".modal button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
-    selectPlanButtons[i].addEventListener('click', function () {
-        // modal.style.display = 'block'
-        // backdrop.style.display = 'block'
-        // modal.className = 'open' //This will actually overwrite the conmplete class
-        modal.classList.add('open');
-        backdrop.style.display = 'block';
-        setTimeout(() => {
-            backdrop.classList.add('open');
-        }, 10);
-    });
+  selectPlanButtons[i].addEventListener("click", function () {
+    // modal.style.display = 'block'
+    // backdrop.style.display = 'block'
+    // modal.className = 'open' //This will actually overwrite the conmplete class
+    modal.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add("open");
+    }, 10);
+  });
 }
 
-backdrop.addEventListener('click', function () {
-    // mobileNav.style.display = 'none';
-    mobileNav.classList.remove('open');
-    closeModal();
-})
+backdrop.addEventListener("click", function () {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  closeModal();
+});
 
 if (modalNoButton) {
-    modalNoButton.addEventListener('click', closeModal);
+  modalNoButton.addEventListener("click", closeModal);
 }
 
 function closeModal() {
-    // modal.style.display = 'none'
-    // backdrop.style.display = 'none'
-    
-    if (modal) {
-        modal.classList.remove('open');
-    }
-    backdrop.classList.remove('open');
-    setTimeout(() => {
-        backdrop.style.display = 'none';
-    }, 200);
+  // modal.style.display = 'none'
+  // backdrop.style.display = 'none'
+
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+  setTimeout(() => {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
-toggleButton.addEventListener('click', function () {
-    // mobileNav.style.display = 'block',
-    // backdrop.style.display = 'block'
+toggleButton.addEventListener("click", function () {
+  // mobileNav.style.display = 'block',
+  // backdrop.style.display = 'block'
 
-    mobileNav.classList.add('open');
-    backdrop.style.display = 'block';
-    setTimeout(() => {
-        backdrop.classList.add('open');
-    }, 10);
-})
+  mobileNav.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(() => {
+    backdrop.classList.add("open");
+  }, 10);
+});
+
+ctaButton.addEventListener('animationstart', (event) => {
+    console.log('Animation started', event);
+});
+
+ctaButton.addEventListener('animationend', (event) => {
+    console.log('Animation ended', event);
+});
+
+ctaButton.addEventListener('animationiteration', (event) => {
+    console.log('Animation itered', event);
+});
